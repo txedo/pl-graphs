19016344c04efd4f4819a55b320d70c815f1f6fc
Txedo <txedo@users.noreply.github.com>
2009-05-05 16:23:46 +0000

    Inicio del semántico

---
+++ 5 - Analizador Semantico/Main.java
@@ -0,0 +1,27 @@
+// Driver for parser
+
+import java_cup.runtime.Symbol;
+import java.io.*;
+
+class Main
+{
+	static boolean do_debug_parse=false;
+	
+	static public void main (String[] args) throws java.io.IOException {
+		parser parser_obj = new parser(new AnaLex(new FileReader(args[0])));
+		Symbol parse_tree = null;
+		try {
+			if (do_debug_parse)
+				parse_tree = parser_obj.debug_parse();
+			else
+				parse_tree = parser_obj.parse();
+		}
+		catch (Exception e) {
+			System.out.println("Analisis INCORRECTO!");
+
+		}
+		finally {
+			System.out.println("Fin del analisis");
+		}
+	}
+}
