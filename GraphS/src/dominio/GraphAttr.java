965dc5dcb6d446a9fa0dd0f3c6fe79c954dd6912
Txedo <txedo@users.noreply.github.com>
2009-05-12 11:30:52 +0000

    FIXED: * Se han incluido todas las clases en el paquete dominio (package dominio;) * Se ha renombrado la clase AtributosGrafo a GraphAttr y se ha ajustado a las nuevas estructuras de datos.

---
+++ GraphS/src/dominio/GraphAttr.java
@@ -0,0 +1,16 @@
+package dominio;
+
+import java.util.ArrayList;
+
+
+public class GraphAttr {
+
+    private ArrayList<Node> nodes;
+    private ArrayList<Edge> edges;
+
+    public GraphAttr (){
+        nodes = new ArrayList<Node>();
+        edges = new ArrayList<Edge>();
+    }
+
+}
