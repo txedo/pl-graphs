a99b8e70ba9bce2ace3ce119be5ad24c1101e8be
Txedo <txedo@users.noreply.github.com>
2009-05-15 22:47:45 +0000

    Semántico con rutinas declaración de nodos y aristas, y conexión de aristas * TODO: lista de operaciones, lista de grafos, timesUsed...

---
+++ GraphS/src/analizador/EntradaTS.java
@@ -0,0 +1,43 @@
+package analizador;
+
+import dominio.Node;
+import dominio.Edge;
+
+public class EntradaTS {
+    private Object variable;
+    private int timesUsed;
+
+    public EntradaTS(Object variable, int timesUsed) {
+        this.variable = variable;
+        this.timesUsed = timesUsed;
+    }
+
+    public int getTimesUsed() {
+        return timesUsed;
+    }
+
+    public Object getVariable() {
+        return variable;
+    }
+
+    public void setTimesUsed(int timesUsed) {
+        this.timesUsed = timesUsed;
+    }
+
+    public void setVariable(Object variable) {
+        this.variable = variable;
+    }
+
+    public int getTipo() {
+        int tipo = -1;
+        
+        if (variable instanceof Node) tipo = parser.NODE;
+        if (variable instanceof Edge) tipo = parser.EDGE;
+        
+        return tipo;
+    }
+
+    public String toString(){
+        return "Variable: " + " timesUsed: " ;
+    }
+}
